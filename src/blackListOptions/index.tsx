efe65050d5a63f00c97fda6254d013f872c273eb
Anborgh <divoannet@gmail.com>
2024-03-30 21:29:42 +0300

    Удаление из списка забаненных

--- src/blackListOptions/index.tsx
+++ src/blackListOptions/index.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 
+import './style.css';
+
 interface IBoardStore {
-  [boardId: string]: {
-    [forumId: string]: {
+  [ boardId: string ]: {
+    [ forumId: string ]: {
       userID: string,
       userName: string,
     }[],
@@ -15,12 +17,33 @@ export function BlackListOptions() {
   const [ boards, setBoards ] = useState({})
 
   const getBoardName = (boardID: string) => {
-    return boards[boardID].boardName || `Форум #${boardID}`;
+    return boards[ boardID ].boardName || `Форум #${ boardID }`;
   }
 
   const getForumName = (boardId: string, forumId: string) => {
-    console.log('boards', boards[boardId], forumId);
-    return boards[boardId]?.forums?.[forumId] || `Раздел #${forumId}`;
+    return boards[ boardId ]?.forums?.[ forumId ] || `Раздел #${ forumId }`;
+  }
+
+  const handleRemoveClick = (boardId: string, forumId: string, user: { userName: string, userID: string }) => {
+    const isConfirmed = confirm(`Разбанить ${user.userName}?`);
+
+    if (!isConfirmed) return;
+
+    const forumBlackList = data[boardId][forumId] || [];
+    const newForumBlackList = forumBlackList.filter(item => item.userID !== user.userID);
+
+    const newData = {
+      ...data,
+      [boardId]: {
+        ...data[boardId],
+        [forumId]: newForumBlackList,
+      },
+    };
+
+    setData(newData);
+    chrome.storage.local.set({
+      ignoreList: newData,
+    });
   }
 
   useEffect(() => {
@@ -38,29 +61,29 @@ export function BlackListOptions() {
   }, []);
 
   return (
-    <div>
-      <ul>
-        {Object.entries(data).map(([boardId, board]) => (
-          <li key={boardId}>
-            {getBoardName(boardId)}
-            <ul>
-              {Object.entries(board).map(([forumId, forum]) => (
-                <li key={forumId}>
-                  {getForumName(boardId, forumId)}
-                  <ul>
-                    {forum.map(user => (
-                      <li key={user.userID}>
-                        {user.userName}
-                        <span>x</span>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul class="blackList">
+      { Object.entries(data).map(([ boardId, board ]) => (
+        <li class="blackListBoardItem" key={ boardId }>
+          { getBoardName(boardId) }
+          <ul class="blackListForum">
+            { Object.entries(board).map(([ forumId, forum ]) => (
+              <li class="blackListForumItem" key={ forumId }>
+                { getForumName(boardId, forumId) }
+                <ul class="blackListUsers">
+                  { forum.map(user => (
+                    <li class="blackListUserItem" key={ user.userID }>
+                      { user.userName }
+                      <span
+                        class="blackListRemoveItem"
+                        onClick={() => handleRemoveClick(boardId, forumId, user)}>x</span>
+                    </li>
+                  )) }
+                </ul>
+              </li>
+            )) }
+          </ul>
+        </li>
+      )) }
+    </ul>
   )
 }
